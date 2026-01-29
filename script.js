f06b15f94841067e807d1b7d962e7cec77ce3b3d
Frank Thaingtham <123851541+FrankThaingtham@users.noreply.github.com>
2026-01-29 18:02:21 +0700

    check winner

--- script.js
+++ script.js
@@ -20,27 +20,63 @@ function createPlayer(name, mark) {
 const Gameboard = gameBoard();
 
 const Game = (() => {
-  let players = [];
-  let currentPlayerIndex = 0;
-  let gameOver = false;
+    let players = [];
+    let currentPlayerIndex = 0;
+    let gameOver = false;
 
-  const startGame = (name1, name2) => {
-    const player1 = createPlayer(name1, "X")
-    const player2 = createPlayer(name2, "O")
-    players = [player1, player2]
-    currentPlayerIndex = 0
-    Gameboard.resetBoard();
-    gameOver = false
-  };
+    const win = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4 ,7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    const checkWinner = (board) => {
+        for (const [a, b, c] of win) {
+        const m = board[a];
+        if (m !== "" && m === board[b] && m === board[c]) return m; // returns "X" or "O"
+        }
+        return null;
+    };
+    const checkTie = (board) => board.every((cell) => cell !== "");
 
-  const playTurn = (index) => {
-    const currentPlayer = players[currentPlayerIndex];
-    if (gameOver) return;
+    const startGame = (name1, name2) => {
+        const player1 = createPlayer(name1, "X")
+        const player2 = createPlayer(name2, "O")
+        players = [player1, player2]
+        currentPlayerIndex = 0
+        Gameboard.resetBoard();
+        gameOver = false
+    };
 
-    const success = Gameboard.placeMark(index, currentPlayer.mark);
-    if (!success) return;
+    const playTurn = (index) => {
+        const currentPlayer = players[currentPlayerIndex];
+        if (gameOver) return;
 
-    currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
-};
+        const success = Gameboard.placeMark(index, currentPlayer.mark);
+        if (!success) return;
+
+        const board = Gameboard.getBoard();
+        const winnerMark = checkWinner(board)
+
+        if (winnerMark) {
+            gameOver = true;
+            result = { type: "win", winner: currentPlayer, mark: winnerMark };
+            return;
+        }
+
+        if (checkTie(board)) {
+            gameOver = true;
+            result = { type: "tie" };
+            return;
+        }
+
+        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+
+    };
+
   return { startGame, playTurn };
 })();
