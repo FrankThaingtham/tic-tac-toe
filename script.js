6816817aae0b06459c41670f38ad4682802464c5
Frank Thaingtham <123851541+FrankThaingtham@users.noreply.github.com>
2026-01-29 17:53:54 +0700

    create Game module

--- script.js
+++ script.js
@@ -18,3 +18,29 @@ function createPlayer(name, mark) {
     return {name, mark}
 }
 const Gameboard = gameBoard();
+
+const Game = (() => {
+  let players = [];
+  let currentPlayerIndex = 0;
+  let gameOver = false;
+
+  const startGame = (name1, name2) => {
+    const player1 = createPlayer(name1, "X")
+    const player2 = createPlayer(name2, "O")
+    players = [player1, player2]
+    currentPlayerIndex = 0
+    Gameboard.resetBoard();
+    gameOver = false
+  };
+
+  const playTurn = (index) => {
+    const currentPlayer = players[currentPlayerIndex];
+    if (gameOver) return;
+
+    const success = Gameboard.placeMark(index, currentPlayer.mark);
+    if (!success) return;
+
+    currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+};
+  return { startGame, playTurn };
+})();
